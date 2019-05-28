4af26bfe633e578137a459344c328062b81833f3
Emile <legendre.emile.27@gmail.com>
2019-05-28 15:41:36 +0200

    Connect the UI sliders to the spin boxes

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -10,10 +10,18 @@ MainWindow::MainWindow(QWidget *parent) :
     // Retrieves the parameter sliders
     QSlider* nbOfStagesSlider = ui->nbOfStagesSlider;
     QSlider* verticesPerStageSlider = ui->verticesPerStageSlider;
+    QSpinBox* nbOfStagesSpinBox = ui->nbOfStagesSpinBox;
+    QSpinBox* verticesPerStageSpinBox = ui->verticesPerStageSpinBox;
 
     // Connects the sliders to the widget
     QObject::connect(nbOfStagesSlider, SIGNAL(valueChanged(int)), ui->GLWidget, SLOT(setNbOfStages(int)));
     QObject::connect(verticesPerStageSlider, SIGNAL(valueChanged(int)), ui->GLWidget, SLOT(setNbOfVerticesPerStage(int)));
+
+    // Connects the spinboxes and the sliders together
+    QObject::connect(nbOfStagesSlider,  SIGNAL(valueChanged(int)), nbOfStagesSpinBox, SLOT(setValue(int)));
+    QObject::connect(nbOfStagesSpinBox,  SIGNAL(valueChanged(int)), nbOfStagesSlider, SLOT(setValue(int)));
+    QObject::connect(verticesPerStageSlider, SIGNAL(valueChanged(int)), verticesPerStageSpinBox, SLOT(setValue(int)));
+    QObject::connect(verticesPerStageSpinBox, SIGNAL(valueChanged(int)), verticesPerStageSlider, SLOT(setValue(int)));
 }
 
 MainWindow::~MainWindow()
