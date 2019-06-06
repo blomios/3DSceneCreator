bd68f2ea8c3bb0aa885dd2443735a396a7eab86b
Emile <legendre.emile.27@gmail.com>
2019-06-06 12:20:03 +0200

    Implement a button allowing the user to select a PNG texture

--- src/mainwidget.h
+++ src/mainwidget.h
@@ -1,58 +1,11 @@
-/****************************************************************************
-**
-** Copyright (C) 2016 The Qt Company Ltd.
-** Contact: https://www.qt.io/licensing/
-**
-** This file is part of the QtCore module of the Qt Toolkit.
-**
-** $QT_BEGIN_LICENSE:BSD$
-** Commercial License Usage
-** Licensees holding valid commercial Qt licenses may use this file in
-** accordance with the commercial license agreement provided with the
-** Software or, alternatively, in accordance with the terms contained in
-** a written agreement between you and The Qt Company. For licensing terms
-** and conditions see https://www.qt.io/terms-conditions. For further
-** information use the contact form at https://www.qt.io/contact-us.
-**
-** BSD License Usage
-** Alternatively, you may use this file under the terms of the BSD license
-** as follows:
-**
-** "Redistribution and use in source and binary forms, with or without
-** modification, are permitted provided that the following conditions are
-** met:
-**   * Redistributions of source code must retain the above copyright
-**     notice, this list of conditions and the following disclaimer.
-**   * Redistributions in binary form must reproduce the above copyright
-**     notice, this list of conditions and the following disclaimer in
-**     the documentation and/or other materials provided with the
-**     distribution.
-**   * Neither the name of The Qt Company Ltd nor the names of its
-**     contributors may be used to endorse or promote products derived
-**     from this software without specific prior written permission.
-**
-**
-** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
-**
-** $QT_END_LICENSE$
-**
-****************************************************************************/
-
 #ifndef MAINWIDGET_H
 #define MAINWIDGET_H
 
 #include "geometryengine.h"
+#include "src/deprecated/Texture.h"
 
+#include <math.h>
+#include <QMouseEvent>
 #include <QOpenGLWidget>
 #include <QOpenGLFunctions>
 #include <QMatrix4x4>
@@ -64,32 +17,53 @@
 
 class GeometryEngine;
 
-class MainWidget : public QOpenGLWidget, protected QOpenGLFunctions
-{
-    Q_OBJECT
+class MainWidget : public QOpenGLWidget, protected QOpenGLFunctions {
+Q_OBJECT
 
 public:
+    // Constructor
     explicit MainWidget(QWidget *parent = nullptr);
+
+    // Destructor
     ~MainWidget() override;
+
+    // Retrieves the geometry engine
     GeometryEngine *getGeometries() const;
+
+    // Updates the bottleneck's position and size, the bottleneck to update is identified with the provided index
     void updateBottlenecks(int index, double position, double xSize, double ySize);
+
+    // Adds a bottleneck with the provided position and size
     void addBottleneck(double position, double xSize, double ySize);
+
+    // Removes the bottleneck identified by the provided index
     void removeBottleneck(int index);
 
+    // Sets the texture of the model
+    void setTexture(QString path);
+
 public slots:
+    // Sets the number of stages of the model
     void setNbOfStages(int stages);
+
+    // Sets the number of vertices per stages of the model
     void setNbOfVerticesPerStage(int nbOfVerticesPerStage);
 
 protected:
     void mousePressEvent(QMouseEvent *e) override;
+
     void mouseReleaseEvent(QMouseEvent *e) override;
+
     void timerEvent(QTimerEvent *e) override;
 
     void initializeGL() override;
+
     void resizeGL(int w, int h) override;
+
     void paintGL() override;
 
     void initShaders();
+
     void initTextures();
 
 private:
@@ -104,12 +78,11 @@ private:
     qreal angularSpeed;
     QQuaternion rotation;
 
+    QOpenGLTexture* texture;
+
     // Object parameters
     int nbOfStages;
     int nbOfVerticesPerStage;
-
-    GLuint texture;
-    QImage img;
 };
 
 #endif // MAINWIDGET_H
